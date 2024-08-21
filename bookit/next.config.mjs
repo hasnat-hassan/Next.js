197a7ba4be70c5203b45a9dc8c6aaad1b6bbf890
hasnat-revnix <hasnathassan397@gmail.com>
2024-08-21 21:01:06 +0500

    initial-commit

---
+++ bookit/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit",
+    DB_URI: "",
+  },
+};
+
+export default nextConfig;
---
+++ bookit/seeder/seeder.ts
@@ -0,0 +1,21 @@
+import room from "@/backend/models/room";
+import mongoose from "mongoose";
+import { rooms } from "./data";
+
+const seedRooms = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/bookit");
+
+    await room.deleteMany();
+    console.log("Rooms are deleted");
+
+    await room.insertMany(rooms);
+    console.log("Rooms are added");
+    process.exit();
+  } catch (error) {
+    console.error(error);
+    process.exit();
+  }
+};
+
+seedRooms();
