9255f9cf6ce1507be097e1c00faef5ae1bd9bfab
hasnat-revnix <hasnathassan397@gmail.com>
2024-09-12 13:12:00 +0500

    url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    API_URL: "https://bookit-lfg3wcja1-hasnats-projects-49956ffc.vercel.app",
+    NEXTAUTH_URL:
+      "https://bookit-lfg3wcja1-hasnats-projects-49956ffc.vercel.app",
+  },
   images: {
     domains: ["res.cloudinary.com"],
   },
